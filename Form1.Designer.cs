11c4dc82fd45949bcc9a015946e1bd9d5d312153
andreea-bumbac <andreeabumbac2002@gmail.com>
2023-04-23 12:20:34 +0300

    t2

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace tema2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewParent = new DataGridView();
+            dataGridViewChild = new DataGridView();
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            dataGridViewParent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewParent.Location = new Point(12, 28);
+            dataGridViewParent.Name = "dataGridViewParent";
+            dataGridViewParent.RowTemplate.Height = 25;
+            dataGridViewParent.Size = new Size(240, 291);
+            dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            dataGridViewChild.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewChild.Location = new Point(268, 28);
+            dataGridViewChild.Name = "dataGridViewChild";
+            dataGridViewChild.RowTemplate.Height = 25;
+            dataGridViewChild.Size = new Size(310, 291);
+            dataGridViewChild.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 325);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 113);
+            panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.Location = new Point(253, 325);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "add";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.Location = new Point(253, 354);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "delete";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 255, 128);
+            button3.Location = new Point(253, 386);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "update";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click_1;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.Location = new Point(253, 415);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 6;
+            button4.Text = "refresh";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(dataGridViewChild);
+            Controls.Add(dataGridViewParent);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewParent;
+        private DataGridView dataGridViewChild;
+        private Panel panel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
